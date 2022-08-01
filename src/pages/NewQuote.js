758420beeae4374e07b6e8a1e53db50fc25f983d
denisbao <denis.bao@cowmed.com.br>
2022-08-01 14:56:31 -0300

    284 - Programmatic (Imperative) Navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,12 @@
+import { useHistory } from 'react-router-dom'
 import QuoteForm from '../components/quotes/QuoteForm'
 
 const NewQuote = () => {
+  const history = useHistory()
+
   function addQuoteHandler(quoteData) {
     console.log(quoteData)
+    history.push('/quotes')
   }
 
   return <QuoteForm onAddQuote={addQuoteHandler} />
